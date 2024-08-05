efc823fdf99f9000a6bf876ba7284ccf2bba2ef9
Pagolin <lisza.zeidler@tu-dresden.de>
2024-08-05 09:57:40 +0200

    make search demo work without Iris

--- code/demos/searching_info.v
+++ code/demos/searching_info.v
@@ -1,4 +1,5 @@
 Require Import String.
+Require Import Arith.
 From lib Require Import thingsToSearch. 
 (* 
    Here we will have a look a vernacular commands, that help us navigate the code, and find informations 
@@ -40,49 +41,63 @@ Locate expr.
 Locate "+".
 
 (*You can search for files*)
-Locate File "types.v".
+Locate File "intro_patterns.v".
 
 (*You can restrict the search to terms ... in this example I'm using a constructor (as the search will tell
 you) so the restriction is not particularly useful*)
 Locate Term LitInt.
 
 (*Will tell you there is non because it's int he file tree but not loaded*)
-Locate Module cbn_logrel.
+Locate Module basics.
+Require Import lib.basics.
 
-(*Will tell you the logical path, because it's a loaded module*)
-Locate Module gmap.
+(*Will tell you the logical path, because it's a loaded module now*)
+Locate Module basics.
 
 
+(**************Print*************)
+
+(*Prints loaded i.e. available library files. It may also print which of them is imported 
+  but that doesn't always seem to work*)
+Print Libraries.
 
 (*Print the definition of defined objects*)
 Print expr.
 
-(*ToDo: Doesn't make sense outside of a proof.*)
-Print All.
+(* Print All.
+   ToDo: Clarify
+   Documentation says : 
+    "This command displays information about 
+    the current state of the environment,
+    including sections and modules."
+   Observable effect: prints only local dependencies (see someProp vs is_val)
+*)
+Section Demosection.
 
-(*The Set expr has no dependencies so it says "Closed under the global context"*)
-Print All Dependencies expr.
+  Definition someProp (n: nat) : Prop := n = 23.
+  Section InnerDemoSection.
 
-(*The lemma says 
-  Lemma of_to_val e v : to_val e = Some v → of_val v = e.
-  Checking it's dependencies will hint you to 
-  Transparent constants (ie. thing that are more or less explicitly used )
-  to_val, of_val, f_equal, False_ind ..
-  and Opaque constants, which in this case is just the lemma itself*)
-Print All Dependencies of_to_val.
-(*Restrict the output of the above*)
-Print Opaque Dependencies of_to_val.
-Print Transparent Dependencies of_to_val.
+    Lemma example : forall (A B C :Prop) (n : nat) (e: expr), 
+        A /\ B -> someProp n -> is_val e -> A \/ B.
+    Proof. 
+      intros.
+      Print All.
+    Abort.
 
-
-(*ToDo: Doesn't make sense outside of a proof*)
-Print Assumptions typed_weakening.
+  End InnerDemoSection.
+End Demosection.
 
 
-(** To check whether a particular proof relies on any additional
-    axioms, use the [Print Assumptions] command:
 
-      Print Assumptions function_equality_ex2
+
+(* Print Assumptions: 
+  "This commands display all the assumptions (axioms, parameters and variables)
+    a theorem or definition depends on. 
+    Especially, it informs on the assumptions with respect 
+    to which the validity of a theorem relies."
+
+    The example is taken from Software Foundations Vol. 1 
+    (https://softwarefoundations.cis.upenn.edu/lf-current/index.html)
 *)
 
 
@@ -94,31 +109,45 @@ Axiom functional_extensionality : forall {X Y: Type}
 Example function_equality_ex2 :
   (fun x => plus x 1) = (fun x => plus 1 x).
 Proof.
-  apply functional_extensionality. intros x.
-  apply add_comm.
+  apply functional_extensionality. 
+  intros x.
+  apply Nat.add_comm.
 Qed.
 
-(* ===>
-     Axioms:
-     functional_extensionality :
-         forall (X Y : Type) (f g : X -> Y),
-                (forall x : X, f x = g x) -> f = g
+Print Assumptions function_equality_ex2.
 
-    (If you try this yourself, you may also see [add_comm] listed as
-    an assumption, depending on whether the copy of [Tactics.v] in the
-    local directory has the proof of [add_comm] filled in.) *)
+(*Variants of [Print Assumptions]: *)
+(*The Set expr has no dependencies so it says "Closed under the global context"*)
+Print All Dependencies expr.
+Print All Dependencies is_val.
+
+(*The lemma says 
+  Lemma of_to_val e v : to_val e = Some v → of_val v = e.
+  Checking it's dependencies will hint you to 
+  Transparent constants (ie. thing that are more or less explicitly used )
+  to_val, of_val, f_equal, False_ind ..
+  and Opaque constants, which in this case is just the lemma itself*)
+Print All Dependencies of_to_val.
+
+(*Restrict the output of the above*)
+Print Opaque Dependencies of_to_val.
+Print Transparent Dependencies of_to_val.
 
 
 (* Lists all the loaded databases 
-   and tactics therein mentioning or relevant to the 
-   big_step relation
-  Hints can be declared ie. written into the databases 
+   and tactics therein mentioning or relevant to the earch term, in
+   this case the step realtion among two expressions.
+   Such hints can be declared ie. written into the databases 
 
-  #[export] Hint Constructors big_step : core.
+    #[export] Hint Constructors big_step : core.
 
-  This writes a constructor hint into the core db.
+   This writes a constructor hint into the core db, so
+   tactics like eauto can automatically find and use it to
+   solve a goal.
 *)
-Print Hint big_step.
+
+Print Hint step.
+
 
 (*basically spells out, what the type definition tells regarding
  implicit arguments*)
@@ -127,20 +156,18 @@ Print Implicit f_equal.
 (*Prints the tactics definition if it is user defined.
   If you ask for a library tactics like <reflexivity> this will probably
   just be a hint to underlying definitions *)
-Print Ltac simplify_val.
+Print Ltac user_def_invert.
 
-(*Print all notations defined in the scope
-  I declared foe expressions i.e. 
-  Declare Scope expr_scope.
-**)
-Print Scope expr_scope.
-(*Try out with overloaded set names**)
 
 (*Gives you a rather long list of all notations
   you have currently available and the scopes they belong to*)
 Print Scopes.
+
+(*Print all notations defined in a particular scope. *)
+Print Scope expr_scope.
+
 (*If you know the notation and want to locate it use: *)
-Locate "(.⊔ y )". 
+Locate "+". 
 
 
 (*Search **)
@@ -148,7 +175,7 @@ Locate "(.⊔ y )".
 (* Search _qualid_ gives all objects (theorems, lemmas etc)  that contain _qualid_.
   Containing means use it/depend on it (not "substring-name-contain" although that often overlaps ) *)
 
-Search rev.
+Search expr.
 
 (** When you Locate a Notation you search for the definition i.e. the rhs ... so you locate the string.
     When you Search the use of a Notation you use it as the parser sees it i.e. not as a string*)
@@ -166,24 +193,48 @@ Search (?x + ?y = ?y + ?x). (*That's the _term-pattern_ search with non-linear r
 (*We can search for strings, iff they are either a valid identifier or a defined Notation 
   string. Again what we'll get from searching is not the defintion but the usages of the term/object we search *)
 
-(* Search "_ + _".          works because it's a defined notation*)
+Search "_ + _".          (*works because it's a defined notation*)
 (* Search "_ + _ = _ + _".  doesn't work because it combines notations*)
 
-Search le_n.
-Search "le_n".
+(* Searching for something particular, 
+   i.e. uniquely identifyable in the current environment
+   or search for it as  a substring *)
+Search Nat.add.
+Search "add".
 
 
 (*Again we can filter by scopes*)
 Search "_ + _"%E. (*anything that uses addition on expressions*)
+Search "_ + _"%nat. (*anything that uses addition on natural numbers*)
 
-(*Searching for the combined usage of dfifferent things*)
+ 
 
-Search expr subst'. (*Searching for uses of expr and subst*)
-Search expr "subst".  (*Searching for uses of expr that have "subst" in their name*)
-Search expr subst' "base". (* when you know the lemma was something with "base" in it*)
+(*Searching for the combined usage of different things*)
 
-Search expr subst' outside lang. (*Again we can search inside and outside of specific modules*) 
+(*Searching for uses of expr and subst*)
+Search expr subst. 
 
-Search expr subst' - is_val. (*Exclude the term "is_val" from the search results*)
-Search expr subst' - "big". (*Exclude anything named ".*big.*" (containing the substring big) from the search results*)
-*)
+(*Searching for uses of expr that have "subst" in their name*)
+Search expr "subst".  
+
+(*When you know you search a lemma about strings that has
+ was something with "eq" in it*)
+Search string "eq". 
+
+(*We can search inside and outside of specific modules*) 
+Search expr subst outside thingsToSearch. (*Nothing to see here*) 
+Search expr subst inside thingsToSearch.
+
+
+Require Import ZArith.
+
+Search Z.add Z.mul "distr".
+(*Excluding the object [Prop] from the search results*)
+Search Z.add Z.mul "distr" -Prop.
+(*Same search, different notation. *)
+Search "+"%Z "*"%Z "distr" -Prop. 
+(*Search definitions that contain the substring "add"
+  but not the substring "distr"*)
+Search Z.add Z.mul "add" -"distr".
+
+   
--- code/helperLib/thingsToSearch.v
+++ code/helperLib/thingsToSearch.v
@@ -1,8 +1,133 @@
-Require Import String.
+(*The examples here are taken from the semantics course material on the separation 
+  logic framework Iris, available at
+  https://plv.mpi-sws.org/semantics-course/
+*)
 
+Require Import String.
 Inductive expr :=
   | Var (x : string)
   | Lam (x : string) (e : expr)
   | App (e1 e2 : expr)
   | LitInt (n: nat)
   | Plus (e1 e2 : expr).
+
+Declare Scope expr_scope.
+Declare Scope val_scope.
+Delimit Scope expr_scope with E.
+Delimit Scope val_scope with V.
+
+Definition is_val (e: expr): Prop := 
+  match e with
+  | LitInt n => True
+  | Lam x e => True
+  | _ => False
+  end.
+
+Inductive val :=
+  | LitIntV (n: nat)
+  | LamV (x : string) (e : expr).
+
+
+(* Turn values into expressions*)
+Definition of_val (v : val) : expr :=
+  match v with
+  | LitIntV n => LitInt n
+  | LamV x e => Lam x e
+  end.
+
+(* try to turn expressions into values *)
+Definition to_val (e : expr) : option val :=
+  match e with
+  | LitInt n => Some (LitIntV n)
+  | Lam x e => Some (LamV x e)
+  | _ => None
+  end.
+
+Lemma to_of_val v : to_val (of_val v) = Some v.
+Proof.
+  destruct v; simpl; reflexivity.
+Qed.
+
+Lemma of_to_val e v : to_val e = Some v -> of_val v = e.
+Proof.
+  destruct e; simpl; try congruence.
+  all: injection 1 as <-; simpl; reflexivity.
+Qed.
+
+
+
+(** *** Substitution *)
+Fixpoint subst (x : string) (es : expr) (e : expr)  : expr :=
+  match e with
+  | LitInt n => LitInt n
+  | Var y => if (eqb x y) then es else Var y
+  | Lam y e =>
+      Lam y (if (eqb x y) then e else subst x es e)
+  | App e1 e2 => App (subst x es e1) (subst x es e2)
+  | Plus e1 e2 => Plus (subst x es e1) (subst x es e2)
+  end.
+
+
+(** *** Small-Step Semantics *)
+(* We use right-to-left evaluation order,
+   which means in a binary term (e.g., e1 + e2),
+   the left side can only be reduced once the right
+   side is fully evaluated (i.e., is a value). *)
+Inductive step : expr -> expr -> Prop :=
+  | StepBeta x e e'  :
+      is_val e' ->
+      step (App (Lam x e) e') (subst x e' e)
+  | StepAppL e1 e1' e2 :
+    is_val e2 ->
+    step e1 e1' ->
+    step (App e1 e2) (App e1' e2)
+  | StepAppR e1 e2 e2' :
+    step e2 e2' ->
+    step (App e1 e2) (App e1 e2')
+  | StepPlusRed (n1 n2 n3: nat) :
+    (n1 + n2)%nat = n3 ->
+    step (Plus (LitInt n1) (LitInt n2)) (LitInt n3)
+  | StepPlusL e1 e1' e2 :
+    is_val e2 ->
+    step e1 e1' ->
+    step (Plus e1 e2) (Plus e1' e2)
+  | StepPlusR e1 e2 e2' :
+    step e2 e2' ->
+    step (Plus e1 e2) (Plus e1 e2').
+
+(* We make the tactic eauto aware of the constructors of [step].
+   Then it can automatically solve goals where we want to prove a step. *)
+#[global] Hint Constructors step : core.
+
+
+(*Simple example of a user defined tactic.*)
+Ltac user_def_invert H :=
+  inversion H; subst; clear H.
+
+
+(*Examples for Scopes*)
+
+Inductive penguins : Set := 
+  | Penguinegg 
+  | Penguin
+  | Huddle (n: nat).
+
+Definition huddle (p1 p2: penguins) : penguins := 
+  match p1 with
+  | Huddle n => match p2 with 
+                 | Huddle m => Huddle (n + m)
+                 | _        => Huddle (S n)
+                end
+  | _        => Huddle (S (S 0))
+  end.
+
+Declare Scope penguins_scope.
+Delimit Scope penguins_scope with Peng.
+Declare Scope expr_scope.
+Delimit Scope expr_scope with E.
+
+Notation "p1 + p2" := (huddle p1 p2)
+                      (at level 50, left associativity)
+                       : penguins_scope.
+
+Notation "e1 + e2" := (Plus e1%E e2%E) : expr_scope.
