770c5e6eb3a208f9355da8a339cc9c8ac8e330f8
Michael Raitza <michael.raitza@barkhauseninstitut.org>
2024-08-06 10:28:39 +0200

    Add flake

--- flake.nix
+++ flake.nix
@@ -1,1 +1,19 @@
-#Todo 
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.default = pkgs.mkShellNoCC {
+      shellHook = ''
+        alias ll="ls -lasi"
+        alias spacemacs="HOME=$(pwd) emacs"
+      '';
+      buildInputs = [
+        pkgs.coqPackages_8_18.coq
+      ];
+    };
+  });
+}
